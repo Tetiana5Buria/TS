58ce7b845d9a787b9be67a1658c946d9caaf3c3f
Tetiana5Buria <poriniya@gmail.com>
2025-11-17 14:00:02 +0200

    after review  fixed some problems

--- src/bagFunction.ts
+++ src/bagFunction.ts
@@ -14,22 +14,25 @@
 } */
 
 interface BigObject {
-  [key: string]:
-    | { cvalue: number | string | BigObject | undefined }
-    | undefined;
+  [key: string]: { cvalue?: number | string | BigObject } | undefined;
 }
 /* function summ(a) { */
 function summ(a: BigObject): number {
-  const x = Object.keys(a).map((k) => {
-    const elem = a[k]?.cvalue;
-    if (typeof elem === undefined) return 2021;
+  const x = Object.keys(a).map((k): number => {
+    const elem = a[k];
+    if (elem === undefined || elem?.cvalue === undefined) return 2021;
     /*  if (typeof elem.cvalue === 'String') return +elem.cvalue || '2021'; */
-    if (typeof elem === "string") return isNaN(+elem) ? 2021 : +elem; //'String'=> 'string' + isNan
+    if (typeof elem.cvalue === "string") {
+      const num = Number(elem.cvalue);
+      return isNaN(num) ? 2021 : num;
+    } //'String'=> 'string' + isNan
     /* if (elem.cvalue.isBigObject !== undefined) return summ(elem); */
-    if (typeof elem === "object" && elem !== null)
-      return summ(elem as BigObject);
+    if (typeof elem.cvalue === "object" && elem.cvalue !== null) {
+      return summ(elem.cvalue);
+    }
+
     /*  return elem.сvalue; */
-    return typeof elem === "number" ? elem : 2021;
+    return elem.cvalue;
   });
   let sum: number = 0;
   /*  for (let i = 0; i < x.lenght; i++) { */
@@ -42,17 +45,27 @@ function summ(a: BigObject): number {
 }
 
 console.log(
-  summ({ s: { cvalue: 1 }, h: { cvalue: "2" }, p: { cvalue: undefined } }) //1+2+2021 = 2024
+  "Нова:" + summ({ s: { cvalue: 1 }, h: { cvalue: "2" }, p: { cvalue: "48" } }) //1+2+48 = 51
 );
 console.log(
   summ({
     r: {
       cvalue: {
         o: { cvalue: 3 },
-        m: { cvalue: "4" },
+        m: { cvalue: "45" },
         a: { cvalue: undefined },
         n: { cvalue: "fgddd2222" },
       },
     },
   })
 ); //0+3+4+2021+2021=4049
+
+console.log(summ({})); //0
+
+console.log(
+  summ({
+    s: { cvalue: "25789" },
+    num: { cvalue: 45 },
+    nan: { cvalue: "" },
+  })
+);
